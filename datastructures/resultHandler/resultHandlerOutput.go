6957a81dfe15c9edeb10231ffdb1bad464d4bf8e
Yvo Elling <Yvo.elling@live.nl>
2019-06-16 13:29:25 +0200

    Implemented remaining data structures

--- datastructures/resultHandler/resultHandlerOutput.go
+++ datastructures/resultHandler/resultHandlerOutput.go
@@ -1,1 +1,20 @@
+//this package contains all the datastructures for the resultHandler software comoponent
 package resultHandler
+
+import (
+	"MD5-Hash-Computer/datastructures/searchController"
+)
+
+//typedef for readability
+type output searchController.WrapperMD5
+
+//resultHandler object class
+//This is an MD5 wrapper as declared before
+type ResultHandlerOutput struct {
+	MD5output output
+}
+
+//returns the data from the MD5 wrapper
+func ( resultHandlerOutput *ResultHandlerOutput) getOutputData() output {
+	return (*resultHandlerOutput).MD5output
+}
