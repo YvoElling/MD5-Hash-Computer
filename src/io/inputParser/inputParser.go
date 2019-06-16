603c59378ffd91ac128834314de99880d1602c5c
Yvo Elling <Yvo.elling@live.nl>
2019-06-16 15:20:33 +0200

    Added headers for inputParser functions

--- src/io/inputParser/inputParser.go
+++ src/io/inputParser/inputParser.go
@@ -1,1 +1,31 @@
 package inputParser
+
+var path string = "input.txt"
+
+//Start the input parser process
+func inputParserMain() {
+
+}
+
+//retrieve file data
+func getFileData() (data []byte){
+
+	return
+}
+
+//evaluates file data
+func evalFileData( data []byte) {
+
+}
+
+//checks if data is comment
+func isComment( input byte) bool{
+
+	return false
+}
+
+//stores the data in object
+func storeInObject( input byte) {
+
+}
+
