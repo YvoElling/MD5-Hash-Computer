247eda5ebbc8f440e64e2d9f89c60e42e168b53f
Yvo Elling <Yvo.elling@live.nl>
2019-06-17 20:33:58 +0200

    Added workerNode headers and exported typealiases from masterNode

--- src/io/outputHandler/outputHandler.go
+++ src/io/outputHandler/outputHandler.go
@@ -6,24 +6,24 @@ import (
 )
 
 //typealiases for readability
-type metadata = *inputParser.Metadata
-type resultHandlerOutput = *resultHandler.ResultHandlerOutput
+type Metadata = *inputParser.Metadata
+type ResultHandlerOutput = *resultHandler.ResultHandlerOutput
 
-//function retrieve metadata from the resulthandler
-func getMetaDataObject () (metadata metadata){
+//function retrieve Metadata from the resulthandler
+func getMetaDataObject () (metadata Metadata){
 
 	return
 }
 
-//function writes metadata to the output file
-func writeMetaData( metadata2 metadata) (success bool){
+//function writes Metadata to the output file
+func writeMetaData( metadata2 Metadata) (success bool){
 
 	return success
 }
 
 //functions gets MD5wrapper object from the resultHandler
 //returns true when written
-func getResultHandlerData ( output resultHandlerOutput ) bool{
+func getResultHandlerData ( output ResultHandlerOutput) bool{
 
 	return true
 }
@@ -34,7 +34,7 @@ func prepareOutputFile() {
 }
 
 //function that writes the MD5 wrapper data to the output .txt file
-func writeToFile( output resultHandlerOutput) bool{
+func writeToFile( output ResultHandlerOutput) bool{
 
 	return true
 }
