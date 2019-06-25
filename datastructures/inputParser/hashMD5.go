0027e97cc75b12b87ede4d75b52b013034bc2e14
Yvo Elling <yvo.elling@solarteameindhoven.nl>
2019-06-25 22:13:13 +0200

    Replaced interface hash.Hash by string equivalent

--- datastructures/inputParser/hashMD5.go
+++ datastructures/inputParser/hashMD5.go
@@ -1,18 +1,18 @@
 // Packages that contains all datastructures related to the inputParser software component
 package inputParser
 
-import "hash"
+type MD5hash string
 
 //HashMD5 struct definition. It is used to store data from the input .txt file
 type HashMD5 struct {
-	md5 hash.Hash
+	md5 MD5hash
 }
 
-func (hashMD5 *HashMD5) SetMd5(md5 hash.Hash) {
+func (hashMD5 *HashMD5) SetMd5(md5 MD5hash) {
 	hashMD5.md5 = md5
 }
 
 //Returns the MD5 hash value from the struct
-func (hashMD5 *HashMD5) getHash() hash.Hash{
+func (hashMD5 *HashMD5) getHash() MD5hash {
 	return (*hashMD5).md5
 }
